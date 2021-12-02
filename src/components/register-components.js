16b2b0bf779aa7cb2c3a5c9b7bf8872365daeb67
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:56:33 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -2,9 +2,9 @@ import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
 
-import ProjectLayout from '../layouts/ProjectLayout',
-import CanvasSection from './CanvasSection',
-import TextSection from './TextSection',
+import ProjectLayout from '../layouts/ProjectLayout'
+import CanvasSection from './CanvasSection'
+import TextSection from './TextSection'
 import ImageSection from './ImageSection'
 
 registerComponents({
