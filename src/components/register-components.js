f72082aadd058f3327e4aa7c226217105554aed7
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:56:12 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -3,7 +3,7 @@ import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
 
 import ProjectLayout from '../layouts/ProjectLayout'
-import ProjectLayout from '../layouts/ProjectLayout'
+import CanvasSection from '../layouts/CanvasSection'
 import ProjectLayout from '../layouts/ProjectLayout'
 import ProjectLayout from '../layouts/ProjectLayout'
 
