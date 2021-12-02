8c41b7166a8d1440c888e707378e52275c3cf560
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:45:29 +0000

    ProjectLayout.js: changed

--- src/layouts/ProjectLayout.js
+++ src/layouts/ProjectLayout.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import dayjs from 'dayjs';
 import Markdown from 'markdown-to-jsx';
-import { getBaseLayoutComponent } from '../../utils/base-layout';
+import { getBaseLayoutComponent } from '@stackbit/components/utils/base-layout';
 import { getComponent } from '../../components-registry';
 
 export default function ProjectLayout(props) {
