8c2187b5b73a1798c3ef1e80ed33b3a88d2276b1
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:51:13 +0000

    TextSection.js: changed

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -10,7 +10,9 @@ export default class TextSection extends React.Component {
 
         return (
             <div className="text-block container container--md">
-                {Markdown(content)}
+                            <Markdown options={{ forceBlock: true }} className="sb-markdown max-w-screen-md mx-auto" data-sb-field-path="markdown_content">
+                                {content}
+                            </Markdown>
             </div>
         );
     }
