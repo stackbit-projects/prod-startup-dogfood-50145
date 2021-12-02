3721bda61aebc61b0d665494825eb5dca70c5beb
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:42:06 +0000

    ProjectLayout.js: changed

--- src/layouts/ProjectLayout.js
+++ src/layouts/ProjectLayout.js
@@ -7,7 +7,7 @@ import { getComponent } from '../../components-registry';
 export default function ProjectLayout(props) {
     const { page, site } = props;
     const BaseLayout = getBaseLayoutComponent(page.baseLayout, site.baseLayout);
-    const sections = page.bottomSections || [];
+    const sections = page.sections || [];
     const dateTimeAttr = dayjs(page.date).format('YYYY-MM-DD HH:mm:ss');
     const formattedDate = dayjs(page.date).format('MMMM D, YYYY');
 
