003909493751ea661533b9daed63fffc009cfcbe
Stackbit Code Editor <projects@stackbit.com>
2021-12-02 10:38:20 +0000

    ProjectLayout.js: changed

--- src/layouts/ProjectLayout.js
+++ src/layouts/ProjectLayout.js
@@ -1,29 +1,46 @@
 import * as React from 'react';
-import { getComponent } from '../../components-registry';
+import dayjs from 'dayjs';
+import Markdown from 'markdown-to-jsx';
 import { getBaseLayoutComponent } from '../../utils/base-layout';
+import { getComponent } from '../../components-registry';
 
-export default function PageLayout(props) {
+export default function PostLayout(props) {
     const { page, site } = props;
     const BaseLayout = getBaseLayoutComponent(page.baseLayout, site.baseLayout);
-    const sections = page.sections || [];
+    const sections = page.bottomSections || [];
+    const dateTimeAttr = dayjs(page.date).format('YYYY-MM-DD HH:mm:ss');
+    const formattedDate = dayjs(page.date).format('MMMM D, YYYY');
 
     return (
         <BaseLayout page={page} site={site}>
-            <main id="main" className="sb-layout sb-page-layout">
-                {page.title && (
-                    <h1 className="sr-only" data-sb-field-path="title">
-                        {page.title}
-                    </h1>
-                )}
+            <main id="main" className="sb-layout sb-post-layout">
+                <article className="colors-a px-4 sm:px-8 py-14 lg:py-20">
+                    <div className="max-w-screen-2xl mx-auto">
+                        <header className="max-w-screen-md mx-auto mb-12 text-center">
+                            <div className="text-lg mb-4">
+                                <time dateTime={dateTimeAttr} data-sb-field-path="date">
+                                    {formattedDate}
+                                </time>
+                            </div>
+                            {page.title && <h1 data-sb-field-path="title">{page.title}</h1>}
+                            {page.author && postAuthor(page.author)}
+                        </header>
+                        {page.markdown_content && (
+                            <Markdown options={{ forceBlock: true }} className="sb-markdown max-w-screen-md mx-auto" data-sb-field-path="markdown_content">
+                                {page.markdown_content}
+                            </Markdown>
+                        )}
+                    </div>
+                </article>
                 {sections.length > 0 && (
-                    <div data-sb-field-path="sections">
+                    <div data-sb-field-path="bottomSections">
                         {sections.map((section, index) => {
                             const Component = getComponent(section.type);
                             if (!Component) {
                                 throw new Error(`no component matching the page section's type: ${section.type}`);
                             }
                             return (
-                                <div key={index} data-sb-field-path={`sections.${index}`}>
+                                <div key={index} data-sb-field-path={`bottomSections.${index}`}>
                                     <Component {...section} />
                                 </div>
                             );
@@ -32,6 +49,18 @@ export default function PageLayout(props) {
                 )}
             </main>
         </BaseLayout>
+    );
+}
+
+function postAuthor(author) {
+    return (
+        <div className="text-lg mt-6">
+            By{' '}
+            <span data-sb-field-path="author">
+                {author.firstName && <span data-sb-field-path=".firstName">{author.firstName}</span>}{' '}
+                {author.lastName && <span data-sb-field-path=".lastName">{author.lastName}</span>}
+            </span>
+        </div>
     );
 }
 
