2bafb4a5407f69d9546a35b48bbc3894946e3f48
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 16:31:59 +0000

    PostFeedSection.tsx: changed

--- src/components/PostFeedSection.tsx
+++ src/components/PostFeedSection.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import dayjs from 'dayjs';
-import { getComponent } from '../../components-registry';
+import { getComponent } from '@stackbit/components';
 import { mapStylesToClassNames as mapStyles } from '../../utils/map-styles-to-class-names';
 import getPageUrlPath from '../../utils/get-page-url-path';
 import Link from '../../utils/link';
